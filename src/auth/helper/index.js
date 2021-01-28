7572ab655621127e3602603a2fb3f45825dfb8af
shashank-k-y <shahsank.sky.8@gmail.com>
2021-01-28 23:49:42 +0530

    signup error handled

--- src/auth/helper/index.js
+++ src/auth/helper/index.js
@@ -2,79 +2,85 @@ import {API} from "../../backend"
 import {cartEmpty} from "../../core/Helper/CartHelper"
 
 export const signup = (user) => {
-    return fetch(`${API}user/`,{
+    return fetch(`${API}user/`, {
         method: "POST",
-        header: {
+        headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-        body:JSON.stringify(user),
+        body: JSON.stringify(user),
     })
-        .then((response)=>{
+        .then((response) => {
             return response.json();
         })
         .catch((err) => console.log(err));
 };
 
-
 export const signin = (user) => {
     const formData = new FormData();
 
-    for(const name in user){
+    for (const name in user) {
         console.log(user[name]);
         formData.append(name, user[name]);
     }
 
-    for (var key of formData.keys()){
+    // const {email, password} = user;
+    // const formData = new FormData();
+    // formData.append('email', email)
+    // formData.append('password', password)
+
+    for (var key of formData.keys()) {
         console.log("MYKEY: ", key);
     }
 
-    return fetch(`${API}user/login/`,{
-        method:"POST",
+    return fetch(`${API}user/login/`, {
+        method: "POST",
+
         body: formData,
     })
-    .then((response)=>{
-        console.log("SUCCESS", response)
-        return response.json();
-    })
-    .catch(err => console.log(err))
-}
+        .then((response) => {
+            console.log("SUCCESS", response);
+            return response.json();
+        })
+        .catch((err) => console.log(err));
+};
 
 export const authenticate = (data, next) => {
-    if(typeof window !== undefined) {
+    if (typeof window !== undefined) {
         localStorage.setItem("jwt", JSON.stringify(data));
         next();
     }
 };
 
 export const isAuthenticated = () => {
-    if (typeof window == undefined){
+    if (typeof window == undefined) {
         return false;
     }
-    if (localStorage.getItem("jwt")){
+    if (localStorage.getItem("jwt")) {
         return JSON.parse(localStorage.getItem("jwt"));
+        //TODO: compare JWT with database json token
     } else {
         return false;
     }
 };
 
-export const signout =( next) => {
-    const userId = isAuthenticated() && isAuthenticated().user.id
+export const signout = (next) => {
+    const userId = isAuthenticated() && isAuthenticated().user.id;
 
     console.log("USERID: ", userId);
 
-    if(typeof window !== undefined){
-        localStorage.removeItem("jwt")
-        cartEmpty(()=>{});
-        // next();
+    if (typeof window !== undefined) {
+        localStorage.removeItem("jwt");
+        cartEmpty(() => { });
+        //next();
 
-        return fetch(`${API}user/logout/${userId}`,{
+        return fetch(`${API}user/logout/${userId}`, {
             method: "GET",
         })
-        .then((response)=>{
-            console.log("Signout success");
-            next();
-        })
+            .then((response) => {
+                console.log("Signout success");
+                next();
+            })
             .catch((err) => console.log(err));
     }
-}
+};
